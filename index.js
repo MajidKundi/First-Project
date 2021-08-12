051d9f01123cbb25d04e8c4d9450a33ee3ceb1cf
Majid Khan <majidkundi1@gmail.com>
2021-08-12 21:10:18 +0500

    First Step

---
+++ index.js
@@ -0,0 +1,50 @@
+// console.log("Come to Papa", 9+3, "You're not my papa" )
+// console.warn("this is warning")
+// console.error("this is error")
+
+// var num1 =36;
+// var num2 =69;
+// console.log(num1 + num2);
+
+// var str1="This is the first step";
+//  var str2='This is the another step';
+
+
+// var obj= {
+// dudi:"first",
+// majid:"second"
+// }
+
+// function showTime(){
+//     var date = new Date();
+//     var h = date.getHours(); // 0 - 23
+//     var m = date.getMinutes(); // 0 - 59
+//     var s = date.getSeconds(); // 0 - 59
+//     var session = "AM";
+    
+//     if(h == 0){
+//         h = 12;
+//     }
+    
+//     if(h > 12){
+//         h = h - 12;
+//         session = "PM";
+//     }
+    
+//     h = (h < 10) ? "0" + h : h;
+//     m = (m < 10) ? "0" + m : m;
+//     s = (s < 10) ? "0" + s : s;
+    
+//     var time = h + ":" + m + ":" + s + " " + session;
+//     document.getElementById("MyClockDisplay").innerText = time;
+//     document.getElementById("MyClockDisplay").textContent = time;
+    
+//     setTimeout(showTime, 1000);
+    
+// }
+
+// showTime();
+
+
+var ran=Math.random();
+console.log(ran);
